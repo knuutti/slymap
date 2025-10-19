a67c49b1fa0864ad81498ab803014b405fc944bd
Eetu Knutars <knuuttisr@gmail.com>
2025-10-19 22:08:29 +0300

    Paris

---
+++ maps/paris/details.js
@@ -0,0 +1,18 @@
+const map_min_zoom = 8;
+const map_max_zoom = 11;
+
+const game_units_per_pixel = 332;
+const game_top_left_x = -468884;
+const game_top_left_y = -392522;
+
+const map_image_width_pixels = 6290;
+const map_image_height_pixels = 5365; 
+const x_offset = 0.073865179437439379243452958292919;
+const y_offset = 0;
+
+const map_sw_x = -1.4917555771096023278370514064 + x_offset;
+const map_sw_y = -1.5809893307468477206595538312318 + y_offset;
+const map_ne_x = 1.418040737148399612027158098933 + x_offset;
+const map_ne_y = 1.3288069835111542192046556741028 + y_offset;
+
+const map_layers = {}
